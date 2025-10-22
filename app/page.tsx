1965b71d297b1643a12f944b4aefca891eb85320
loihnt05 <23520869@gm.uit.edu.vn>
2025-10-22 13:56:46 +0700

    auth0 with profile test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+"use client";
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
 
 export default function Home() {
+  const { user, isLoading } = useUser();
   return (
     <div>
-      <Button>Helo</Button>
+      {isLoading && <p>Loading...</p>}
+      {!user && <Link href="/auth/login">Login</Link>}
+      {user && (
+        <div style={{ textAlign: "center" }}>
+          <a href="/auth/logout">Logout</a>
+
+          <img
+            src={user.picture}
+            alt="Profile"
+            style={{ borderRadius: "50%", width: "80px", height: "80px" }}
+          />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
+}
+
+export function Profile() {
+  return <></>;
 }
