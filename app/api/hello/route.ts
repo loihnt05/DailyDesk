944b2660510758aa77f34817b8426583f2bc3129
huyphamnhat <huy.pham.nhat@omniaintranet.com>
2025-10-23 13:20:10 +0700

    setup tanstack query

---
+++ app/api/hello/route.ts
@@ -0,0 +1,15 @@
+import db from "@/lib/db";
+import { usersTable } from "@/lib/db/schema";
+
+export async function GET(request: Request) {
+  return Response.json(await db.select().from(usersTable));
+}
+
+export async function POST(request: Request) {
+  const { name, age, email } = await request.json();
+  const result = await db
+    .insert(usersTable)
+    .values({ name, age, email })
+    .returning();
+  return Response.json(result);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["hello"],
+    queryFn: async () => {
+      const res = await fetch("/api/hello");
+      return await res.json();
+    },
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error!</div>;
+  }
+
+  async function createUser() {
+    const res = await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "John", age: 30, email: "john@example.com" }),
+    });
+  }
+
   return (
     <div>
-      <Button>Helo</Button>
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button onClick={createUser}>Hello</Button>
     </div>
   );
 }
