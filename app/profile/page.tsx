19e99291f7f91f6f68843f71812a70d0cd3a993d
loihnt05 <23520869@gm.uit.edu.vn>
2025-10-27 21:38:13 +0700

    add profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+
+export default function Profile() {
+  const { user, isLoading } = useUser();
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {!user && <Link href="/auth/login?returnTo=/profile">Login</Link>}
+      {user && (
+        <div style={{ textAlign: "center" }}>
+          <Link href="/auth/logout">Logout</Link>
+
+          <img
+            src={user.picture}
+            alt="Profile"
+            style={{ borderRadius: "50%", width: "80px", height: "80px" }}
+          />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
